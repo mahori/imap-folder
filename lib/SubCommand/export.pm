1b285d4a3dee02ba51173b692084454e13a3b7f4
mahori <4198737+mahori@users.noreply.github.com>
2021-09-05 11:39:05 +0900

    exportサブコマンドを実装

---
+++ lib/SubCommand/export.pm
@@ -0,0 +1,53 @@
+package SubCommand::export;
+
+use strict;
+use warnings;
+use feature qw( say );
+use Encode qw( from_to );
+use Encode::IMAPUTF7;
+use File::Spec::Functions qw( catfile );
+
+use FindBin qw( $RealBin );
+use lib "$RealBin/lib";
+use base qw( SubCommand::Base );
+
+sub execute {
+  my ( $self, $folder, $directory ) = @_;
+
+  unless ( defined $folder && defined $directory ) {
+    return;
+  }
+
+  my $client = $self->{client};
+
+  from_to( $folder, 'UTF-8', 'IMAP-UTF-7' );
+  $client->select( $folder ) or die $!;
+
+  my @messages = $client->sort( 'DATE', 'US-ASCII', 'ALL' ) or die $!;
+  unless ( @messages ) {
+    return;
+  }
+
+  my $count  = scalar @messages;
+  my $length = length $count;
+  my $format = sprintf '%%0%dd', $length;
+
+  unless ( -d $directory ) {
+    mkdir $directory or die $!;
+  }
+
+  my $index = 1;
+  foreach my $message ( @messages ) {
+    my $basename = sprintf $format, $index;
+    my $uid      = sprintf $format, $message;
+
+    say STDERR "$basename : $uid";
+
+    my $filename = catfile( $directory, $basename );
+    $client->message_to_file( $filename, $message ) or die $!;
+
+    $index++;
+  }
+}
+
+1;
