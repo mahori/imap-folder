bef4c14fac112c927b06c9d0ace99350ccc4e25b
mahori <4198737+mahori@users.noreply.github.com>
2021-09-05 10:29:10 +0900

    messages_dateサブコマンドを実装

---
+++ lib/SubCommand/messages_date.pm
@@ -0,0 +1,35 @@
+package SubCommand::messages_date;
+
+use strict;
+use warnings;
+use feature qw( say );
+use Encode qw( from_to );
+use Encode::IMAPUTF7;
+
+use FindBin qw( $RealBin );
+use lib "$RealBin/lib";
+use base qw( SubCommand::Base );
+
+sub execute {
+  my ( $self, $folder ) = @_;
+
+  unless ( defined $folder ) {
+    return;
+  }
+
+  my $client = $self->{client};
+
+  from_to( $folder, 'UTF-8', 'IMAP-UTF-7' );
+  $client->select( $folder ) or die $!;
+
+  my @messages = $client->sort( 'DATE', 'US-ASCII', 'ALL' ) or die $!;
+  unless ( @messages ) {
+    return;
+  }
+
+  foreach my $message ( @messages ) {
+    say $message;
+  }
+}
+
+1;
