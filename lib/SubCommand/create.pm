cd1bf3e332c393712dec44284376b75e842c9fc7
mahori <4198737+mahori@users.noreply.github.com>
2021-09-05 12:44:26 +0900

    createサブコマンドを実装

---
+++ lib/SubCommand/create.pm
@@ -0,0 +1,25 @@
+package SubCommand::create;
+
+use strict;
+use warnings;
+use Encode qw( from_to );
+use Encode::IMAPUTF7;
+
+use FindBin qw( $RealBin );
+use lib "$RealBin/lib";
+use base qw( SubCommand::Base );
+
+sub execute {
+  my ( $self, $folder ) = @_;
+
+  unless ( defined $folder ) {
+    return;
+  }
+
+  my $client = $self->{client};
+
+  from_to( $folder, 'UTF-8', 'IMAP-UTF-7' );
+  $client->create( $folder ) or die $!;
+}
+
+1;
