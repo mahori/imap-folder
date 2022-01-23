760541d33f2c255e979c1e52f5e30ba9f7ae194b
mahori <4198737+mahori@users.noreply.github.com>
2022-01-23 20:22:43 +0900

    trash_allサブコマンドを実装

---
+++ lib/trash_all.pm
@@ -0,0 +1,50 @@
+package trash_all;
+
+use strict;
+use warnings;
+use Encode qw( from_to );
+use Encode::IMAPUTF7;
+
+use FindBin qw( $RealBin );
+use lib "$RealBin/lib";
+use base qw( Command );
+
+sub execute {
+  my ( $self, $folder ) = @_;
+
+  unless ( defined $folder ) {
+    return;
+  }
+
+  my $client = $self->{client};
+
+  from_to( $folder, 'UTF-8', 'IMAP-UTF-7' );
+  $client->select( $folder ) or die $@;
+
+  my $count = $client->message_count;
+  unless ( defined $count ) {
+    die $@;
+  }
+  if ( $count == 0 ) {
+    return;
+  }
+
+  my @messages = $client->messages or die $@;
+  unless ( @messages ) {
+    return;
+  }
+
+  my @folders = $client->folders_hash or die $@;
+  my @trash   = grep { grep { $_ eq '\\Trash' } @{$_->{attrs}} } @folders;
+  unless ( scalar @trash == 1 ) {
+    return;
+  }
+
+  foreach my $message ( @messages ) {
+    $client->move( $trash[0]->{name}, $message ) or die $@;
+  }
+
+  $client->expunge( $folder ) or die $@;
+}
+
+1;
